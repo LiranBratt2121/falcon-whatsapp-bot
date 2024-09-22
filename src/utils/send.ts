496feb22c4cd7dd72bfa60889b8783c08e85b1c6
LiranBratt2121 <brattlirannin@gmail.com>
2024-09-23 00:52:22 +0300

    Add options parameters

--- src/utils/send.ts
+++ src/utils/send.ts
@@ -1,10 +1,10 @@
-import { Client, Message } from "whatsapp-web.js";
+import { Client, Message, MessageContent, MessageSendOptions } from "whatsapp-web.js";
 
-export const sendMessage = (client: Client, chatId: string, content: string): void => {
-    client.sendMessage(chatId, content);
+export const sendMessage = (client: Client, chatId: string, content: string, options?: MessageSendOptions): void => {
+    client.sendMessage(chatId, content, options);
 }
 
-export const replyMessage = (message: Message, content: string): void => {
-    message.reply(content);
+export const replyMessage = (message: Message, content: string | MessageContent, options?: MessageSendOptions): void => {
+    message.reply(content, message.from, options);
 }
 
