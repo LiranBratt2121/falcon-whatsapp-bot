3b1a9082c5d6b58b7d688bc41e04dd8aa8020cc8
LiranBratt2121 <brattlirannin@gmail.com>
2024-09-23 07:09:51 +0300

    Add logs for more info

--- src/utils/quotes.ts
+++ src/utils/quotes.ts
@@ -2,6 +2,7 @@ import { Message } from "whatsapp-web.js";
 
 export const getQuotedMessage = async (message: Message): Promise<Message | null> => {
     if (!message.hasQuotedMsg) {
+        console.log("No quoted Message!");
         return null;
     }
 
