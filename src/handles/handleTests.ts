32bf75e74316b8465a2e6835ff4c83b0e74e6440
LiranBratt2121 <brattlirannin@gmail.com>
2024-09-25 02:55:11 +0300

    Make boolean return types to default reply

--- src/handles/handleTests.ts
+++ src/handles/handleTests.ts
@@ -1,11 +1,14 @@
 import { Message } from "whatsapp-web.js";
 import { replyMessage } from "../utils/send"
 
-export const handleDefault = (message: Message): void => {
+export const handleDefault = (message: Message): boolean => {
     const content = `Message not supported by the bot.`;
     if (message.body.startsWith('!')) {
         replyMessage(message, content);
+        return true;
     }
+
+    return false;
 }
 
 export const handlePing = (message: Message): void => {
