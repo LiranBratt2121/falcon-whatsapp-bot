1a4503d031f48082cad1ded694df3beda8866f1d
LiranBratt2121 <brattlirannin@gmail.com>
2024-09-25 02:57:47 +0300

    Add time convention types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,3 +1,17 @@
+export enum TimeConvention {
+    SECS = Math.pow(10, 3),
+    MINS = SECS * 60,
+    HOURS = 3.6e6,
+    DAYS = 8.64e7,
+    NONE = -1
+}
+
+export type TimeContainer = {
+    timeConvention: TimeConvention;
+    timeTitle: string;
+    timeToWait: number;
+}
+
 export type ChatId = string;
 export type ActionWord = string;
 export type ActionWordContent = string;
