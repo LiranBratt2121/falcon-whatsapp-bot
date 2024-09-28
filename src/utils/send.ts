92fdb54eb46425f87a7e59ad3f1f4b01ddd40006
LiranBratt2121 <brattlirannin@gmail.com>
2024-09-28 04:03:06 +0300

    Add sendMediaAsSticker function

--- src/utils/send.ts
+++ src/utils/send.ts
@@ -1,5 +1,5 @@
-import { Client, Message, MessageContent, MessageSendOptions } from "whatsapp-web.js";
-import { TimeConvention } from "./types";
+import { Client, Message, MessageContent, MessageMedia, MessageSendOptions } from "whatsapp-web.js";
+import { ImageTypes, TimeConvention } from "./types";
 
 export const sendMessage = (client: Client, chatId: string, content: string, options?: MessageSendOptions): void => {
     client.sendMessage(chatId, content, options);
@@ -23,4 +23,9 @@ export const sendDelyedReply = (convention: TimeConvention, time: number, messag
 export const sendDelyedReplyWithMsg = (convention: TimeConvention, time: number, message: Message, quotedMessage: Message, content: string | MessageContent, options?: MessageSendOptions): void => {
     const waitTimeInMS = convention * time;
     setTimeout(() => message.reply(content, message.from, options), waitTimeInMS);
+}
+
+export const sendPathAsSticker = (imagePath: string, type: ImageTypes, message: Message) => {
+    const media = new MessageMedia(`image/${type}`, imagePath, `image.${type}`, imagePath.length);
+    replyMessage(message, media, { sendMediaAsSticker: true });
 }
